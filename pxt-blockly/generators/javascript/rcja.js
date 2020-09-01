efffde7a3ba0572bc601dfa3dc8437175abc1ade
DUHsoALEX <z5206696@ad.unsw.edu.au>
2020-09-01 15:33:18 +1000

    created mvp blocks

--- pxt-blockly/generators/javascript/rcja.js
+++ pxt-blockly/generators/javascript/rcja.js
@@ -18,3 +18,30 @@ Blockly.JavaScript['motor_set_speed'] = function(block) {
   var code = 'setMotorSpeed(' + arg + ');\n';
   return code;
 };
+
+Blockly.JavaScript['ball_get_angle'] = function(block) {
+  var code = 'getBallAngle()';
+  return [code, Blockly.JavaScript.ORDER_NONE];
+};
+
+Blockly.JavaScript['ball_get_distance'] = function(block) {
+  var code = 'getBallDistance()';
+  return [code, Blockly.JavaScript.ORDER_NONE];
+};
+
+Blockly.JavaScript['compass_get_heading'] = function(block) {
+  var code = 'getCompassHeading()';
+  return [code, Blockly.JavaScript.ORDER_NONE];
+};
+
+Blockly.JavaScript['print'] = function(block) {
+  var arg = Blockly.JavaScript.valueToCode(block, 'print_input', Blockly.JavaScript.ORDER_ATOMIC);
+  var code = 'print(' + arg + ');\n';
+  return code;
+};
+
+Blockly.JavaScript['on_program_start'] = function(block) {
+  var arg = Blockly.JavaScript.statementToCode(block, 'start_input');
+  var code = 'onProgramStart() {\n' + arg + '}\n';
+  return code;
+};
