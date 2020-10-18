4d5a35989cc7f38273ae749a151dc48b6d5d4c47
Kris <kris.choy1@gmail.com>
2020-10-18 14:14:50 +1100

    Split environments properly

--- scripts/sim-setup.js
+++ scripts/sim-setup.js
@@ -7,24 +7,23 @@ var Bodies = Matter.Bodies,
 
 let title = document.getElementsByTagName("title")[0].innerHTML;
 
-// Define robots on the field
-let one = new DualBot('blue',-75, 130, fieldWidth, fieldHeight),
-two = new TriBot('blue',75, 130, fieldWidth, fieldHeight),
-three = new UniBot('yellow',-75,130, fieldWidth, fieldHeight);
-
-let robots = [one, two, three];
-window.One = one;
-window.Two = two;
-window.Three = three;
-
 // define the ball
 let ball = Bodies.circle(fieldWidth/2, fieldHeight/2, 10, {
     frictionAir: 0.02,
     render: {fillStyle: '#f95a00'}
 });
-window.Ball = ball;
 
 if (title === 'Matter.js Demo') {
+    // Define robots on the field
+    let one = new DualBot('blue', -75, 130, fieldWidth, fieldHeight),
+        two = new TriBot('blue', 75, 130, fieldWidth, fieldHeight),
+        three = new UniBot('yellow', -75, 130, fieldWidth, fieldHeight);
+
+    let robots = [one, two, three];
+    window.One = one;
+    window.Two = two;
+    window.Three = three;
+    window.Ball = ball;
     // Create a new simulation if selected
     window.Example = window.Example || {};
     window.Example.soccer = function() {
@@ -32,8 +31,15 @@ if (title === 'Matter.js Demo') {
         return sim.demo();
     };
 } else {
-    // Assume it is the actual interface
-    // Create a new simulation
+    // Real interface
+    // Define robots on the field
+    let blue1 = new DualBot('blue', -75, 130, fieldWidth, fieldHeight);
+    let blue2 = new DualBot('blue', 75, 130, fieldWidth, fieldHeight);
+    // TODO: Pass these into simcontrols, rather than using window
+    window.One = blue1;
+    window.Two = blue2;
+    window.Ball = ball;
+    let robots = [blue1, blue2];
     var sim = new SoccerSim(document.getElementById('matterjs'), robots, ball, fieldWidth, fieldHeight);
     // Engine.run(sim.engine);
     // Render.run(sim.render);
