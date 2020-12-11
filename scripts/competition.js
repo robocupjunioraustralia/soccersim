3453d67fe001bef6a22f6a8b3f2fe10de7115fa8
DUHsoALEX <z5206696@ad.unsw.edu.au>
2020-12-11 21:39:53 +1100

    scoring modal functionality implemented

---
+++ scripts/competition.js
@@ -0,0 +1,74 @@
+;(function() {
+  let competition = {};
+  competition.scores = {
+    yellow: 0,
+    blue: 0
+  };
+
+  competition.tempScores = {
+    yellow: 0,
+    blue: 0
+  };
+
+  let blueScore = document.getElementById("blue-score");
+  let yellowScore = document.getElementById("yellow-score");
+  
+  let blueModalScore = document.getElementById("blue-modal-score");
+  let yellowModalScore = document.getElementById("yellow-modal-score");
+
+  let scoringSetup = document.getElementById('scoring-setup');
+  scoringSetup.addEventListener('click', function() {
+    competition.tempScores.blue = competition.scores.blue;
+    competition.tempScores.yellow = competition.scores.yellow;
+
+    blueScore.textContent = competition.scores.blue;
+    yellowScore.textContent = competition.scores.yellow;
+    blueModalScore.textContent = competition.scores.blue;
+    yellowModalScore.textContent = competition.scores.yellow;
+  });
+  
+  let blueReset = document.getElementById("blue-reset");
+  blueReset.addEventListener('click', function() {
+    competition.tempScores.blue = 0;
+    blueModalScore.textContent = competition.tempScores.blue;
+  });
+
+  let yellowReset = document.getElementById("yellow-reset");
+  yellowReset.addEventListener('click', function() {
+    competition.tempScores.yellow = 0;
+    yellowModalScore.textContent = competition.tempScores.yellow;
+  });
+
+  let bluePlus = document.getElementById("blue-plus");
+  bluePlus.addEventListener('click', function() {
+    competition.tempScores.blue++;
+    blueModalScore.textContent = competition.tempScores.blue;
+  });
+
+  let yellowPlus = document.getElementById("yellow-plus");
+  yellowPlus.addEventListener('click', function() {
+    competition.tempScores.yellow++;
+    yellowModalScore.textContent = competition.tempScores.yellow;
+  });
+
+  let blueMinus = document.getElementById("blue-minus");
+  blueMinus.addEventListener('click', function() {
+    competition.tempScores.blue--;
+    blueModalScore.textContent = competition.tempScores.blue;
+  });
+
+  let yellowMinus = document.getElementById("yellow-minus");
+  yellowMinus.addEventListener('click', function() {
+    competition.tempScores.yellow--;
+    yellowModalScore.textContent = competition.tempScores.yellow;
+  });
+
+  let scoringSave = document.getElementById('scoring-save');
+  scoringSave.addEventListener('click', function() {
+    competition.scores.blue = competition.tempScores.blue;
+    competition.scores.yellow = competition.tempScores.yellow;
+
+    blueScore.textContent = competition.scores.blue;
+    yellowScore.textContent = competition.scores.yellow;
+  });
+})();
